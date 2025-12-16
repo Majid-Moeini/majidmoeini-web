4cc7ebd140d358dc88c75427fc35dc75910f6c44
Majid Moeini <118059574+Majid-Moeini@users.noreply.github.com>
2025-12-16 18:03:52 +0330

    hearo 99%

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 
 export default function Hero() {
     return (
-        <section className="relative  h-fit overflow-hidden">
+        <section className="relative h-fit overflow-hidden">
             {/* background */}
             <Image
                 src="/images/hero-bg.png"
@@ -14,29 +14,31 @@ export default function Hero() {
             />
 
             {/* content */}
-            <div className="relative flex flex-col lg:flex-row z-10 h-full mt-20 md:mt-50 items-center">
+
+            <div className="relative flex flex-col lg:flex-row z-10 h-fit mt-[60px] md:mt-[200px] items-center lg:-translate-x-[15%]">
 
                 {/* left content */}
-                <div className="flex flex-1 lg:w-[40%] flex-col justify-center gap-4 lg:mr-[14%] mb-8 h-full items-center lg:items-start ">
+
+                <div className="flex lg:w-[60%] lg:max-w-[500px] flex-col justify-center gap-4 h-full items-center lg:items-start lg:ml-auto mb-15 ">
                     <p className="inline-block rounded-3xl bg-white px-4 py-2 mb-3 text-xs text-gray-800 ">
                         اپلیکیشن کمک دست راننده و مکانیک
                     </p>
 
-                    <h1 className="font-modam text-4xl font-black text-gray-800 px-3 md:px-0 text-center md:text-start">
-                        ایکپ، سامانه دستیار هوشمند خودرو
+                    <h1 className="font-modam text-4xl font-black text-gray-800 px-3 md:px-0 text-center md:text-start leading-snug">
+                        ایکپ، سامانه دستیار<br /> هوشمند خودرو
                     </h1>
 
                     <p className="text-2xl text-gray-800 py-2">
                         مکانیک یاب و عیب یاب
                     </p>
 
-                    <div className="mt-6 flex gap-6 ">
+                    <div className=" flex gap-8 w-full h-[50px] justify-end">
                         <Button
                             asChild
-                            className="w-40 bg-white"
+                            className=" bg-white w-[200px] h-full hover:-translate-y-0.5 text-lg"
                         >
                             <a href="https://myket.ir" target="_blank">
-                                <span className="flex items-center gap-2">
+                                <span className="flex items-center gap-2 text-[1.125rem]">
                                     دانلود از مایکت
                                     <Image
                                         src="/images/icons/Myket.png"
@@ -50,7 +52,7 @@ export default function Hero() {
 
                         <Button
                             asChild
-                            className="w-40 bg-white"
+                            className=" bg-white w-[200px] h-full hover:-translate-y-0.5 text-lg"
                         >
                             <a href="https://cafebazaar.ir" target="_blank">
                                 <span className="flex items-center gap-2">
@@ -69,16 +71,16 @@ export default function Hero() {
 
 
                 {/* right image */}
-                <div className="flex flex-1 w-full h-fit mr-auto flex-col justify-end ">
+                <div className="relative min-w-4xl h-[335px] md:h-[435px] -translate-x-[8%] xl:-translate-x-[7%]">
                     <Image
                         src="/images/personPic.png"
                         alt="icap-person"
-                        width={1024}
-                        height={535}
+                        fill
                         priority
-                        className=" lg:-translate-x-[35%] "
+                        className="object-contain object-bottom"
                     />
                 </div>
+
             </div>
         </section>
     )
