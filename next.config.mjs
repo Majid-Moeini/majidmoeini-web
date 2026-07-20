5b1aba481addafddcdb211bd125561ff8bfbe2d7
Majid Moeini <118059574+Majid-Moeini@users.noreply.github.com>
2026-07-20 22:08:46 +0330

    ad config options

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
-
-  //for static build :
-  output: "export",
-
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
